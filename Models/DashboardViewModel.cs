08ee6b364ffe6c6c3e013cef6421ce6e8f227933
yasser-mohamed1 <y4ssermoh4med@gmail.com>
2025-06-03 11:58:10 +0300

    Add DashboardViewModel and TaskViewModel classes with properties for task management

---
+++ Models/DashboardViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskManager.Models
+{
+    public class DashboardViewModel
+    {
+        public int TotalTasks { get; set; }
+        public int CompletedTasks { get; set; }
+        public int PendingTasks { get; set; }
+        public int OverdueTasks { get; set; }
+        public List<TaskItem> RecentTasks { get; set; } = new();
+        public List<TaskItem> UpcomingTasks { get; set; } = new();
+    }
+}
--- Models/TaskViewModel.cs
+++ Models/TaskViewModel.cs
@@ -25,14 +25,4 @@ namespace TaskManager.Models
 
         public bool IsCompleted { get; set; }
     }
-
-    public class DashboardViewModel
-    {
-        public int TotalTasks { get; set; }
-        public int CompletedTasks { get; set; }
-        public int PendingTasks { get; set; }
-        public int OverdueTasks { get; set; }
-        public List<TaskItem> RecentTasks { get; set; } = new();
-        public List<TaskItem> UpcomingTasks { get; set; } = new();
-    }
 }
